9be5081a59e63266b1117929e7169630eae5bd89
ammargoher94@mail.com <ammargoher94@mail.com>
2024-08-11 20:31:59 +0300

    last modify

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ import cartRouter from './routes/cartRouts.js';
 const app = express();
 app.use(
   cors({
-    origin: '*',
+    origin: [
+      'https://frondend-eshop.vercel.app',
+      'https://frondend-eshop.vercel.app',
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   })
